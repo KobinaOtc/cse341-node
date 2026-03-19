a25060f16e0abf5a9845c75cbbb5850adb02bdba
KobinaOtc <kobinaotchereadjoku@gmail.com>
2026-03-19 19:05:21 +0000

    update

---
+++ validator.js
@@ -0,0 +1,26 @@
+const { body, validationResult } = require('express-validator');
+const userValidationRules = () => {
+    return [
+        body('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'), // Validate that username is at least 5 characters long
+        body('email').isEmail().withMessage('Please provide a valid email address'), // Validate that email is in a valid format
+        body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long') // Validate that password is at least 8 characters long
+    ]
+}
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    const extractedErrors = [];
+    errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+    return res.status(422).json({
+        errors: extractedErrors,
+    });
+}
+
+module.exports = {
+    userValidationRules,
+    validate,
+}
